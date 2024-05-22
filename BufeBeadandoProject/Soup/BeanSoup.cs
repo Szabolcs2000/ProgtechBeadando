8f1115955212f14919458d228bfbc7ec8dfe3358
Varga Szabolcs <szabolcs11222@gmail.com>
2024-05-22 19:41:17 +0200

    Első levesek felvétele teszt (Laptop)

---
+++ BufeBeadandoProject/Soup/BeanSoup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BufeBeadandoProject.Soup
+{
+    internal class BeanSoup : Soup
+    {
+        public BeanSoup(string name, int price) : base(name, price)
+        {
+            name = "Bableves";
+            price = 500;
+        }
+    }
+}
---
+++ BufeBeadandoProject/Soup/Soup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BufeBeadandoProject.Soup
+{
+    internal abstract class Soup
+    {
+        public Soup(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; private set; }
+
+        public int Price { get; private set; }
+
+    }
+}
